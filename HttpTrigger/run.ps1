7f637e2e362b7275cedd5e8c886bdf2db0716c97
lowbrain <83059975+lowbrain@users.noreply.github.com>
2025-05-04 09:25:32 +0900

    mg connect test

--- HttpTrigger/run.ps1
+++ HttpTrigger/run.ps1
@@ -6,6 +6,8 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
+Connect-MgGraph -Identity
+
 # Interact with query parameters or the body of the request.
 $name = $Request.Query.Name
 if (-not $name) {
